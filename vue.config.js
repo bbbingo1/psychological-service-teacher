393da89ba0aa576f87e2b7088309be9910535423
bbbingo1 <846790344@qq.com>
2021-05-13 15:35:06 +0800

    chore: 更改静态资源配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  publicPath:"./",
+  publicPath: './',
+  outputDir: "dist",
+  assetsDir: 'static',
+  pwa: {
+    iconPaths: {
+      favicon32: 'static/img/titleicon.jpg',
+      favicon16: 'static/img/titleicon.jpg',
+      appleTouchIcon: 'static/img/titleicon.jpg',
+      maskIcon: 'static/img/titleicon.jpg',
+      msTileImage: 'static/img/titleicon.jpg'
+    }
+  },
   devServer: {
     proxy: {
       '/api': {
